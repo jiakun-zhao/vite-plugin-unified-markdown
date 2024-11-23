87d92b4160ee289a1cf6032ecfaa595d6349d694
Jiakun Zhao <50269993+jiakun-zhao@users.noreply.github.com>
2024-11-23 20:25:51 +0800

    chore: update

--- src/markdown.ts
+++ src/markdown.ts
@@ -1,7 +1,7 @@
 import type { PluggableList } from 'unified'
 import { isString } from '@antfu/utils'
 import rehypeStringify from 'rehype-stringify'
-import remarkMdc from 'remark-mdc'
+import remarkMdc, { parseFrontMatter } from 'remark-mdc'
 import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
 import { unified } from 'unified'
@@ -11,21 +11,33 @@ declare module 'vfile' {
   interface DataMap {
     script: string
     style: Style
+    frontmatter: Frontmatter
   }
 }
 
+type Frontmatter = ReturnType<typeof parseFrontMatter>['data']
 type Style = string | { css: string, scoped?: boolean }
+
+interface TransformFnValue {
+  frontmatter: Frontmatter
+  style: Style
+  script: string
+  template: string
+}
 
 interface Input {
   content: string
   remarkPlugins?: RemarkPlugins
   rehypePlugins?: RehypePlugins
+  transform?: (value: TransformFnValue) => Omit<TransformFnValue, 'frontmatter'>
   script?: string
   style?: Style
 }
 
 export async function toVueComponent(input: Input) {
-  const { value, data: { script, style } } = await unified()
+  const { content, data: frontmatter } = parseFrontMatter(input.content)
+
+  const result = await unified()
     .use(remarkParse)
     .use(input.remarkPlugins as PluggableList ?? [])
     .use(remarkRehype)
@@ -33,16 +45,18 @@ export async function toVueComponent(input: Input) {
     .use(input.rehypePlugins as PluggableList ?? [])
     .use(rehypeStringify)
     .process({
-      value: input.content,
+      value: content,
       data: {
+        frontmatter,
         script: input.script ?? '',
         style: input.style ?? '',
       },
     })
 
-  const css = isString(style)
+  const value = { frontmatter, script: result.data.script!, style: result.data.style!, template: result.value.toString() }
+  const { script, style, template } = input.transform ? input.transform(value) : value
+  const { css, scoped } = isString(style)
     ? { css: style, scoped: '' }
     : { css: style?.css ?? '', scoped: style?.scoped ? ' scoped' : '' }
-
-  return `<template>${value}</template><script>${script}</script><style${css.scoped}>${css.css}</style>`
+  return `<template>${template}</template><script>${script}</script><style${scoped}>${css}</style>`
 }
